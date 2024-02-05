004bb1c07b1f46c1db00d41c21a65cfa1d2cc283
Christian Kirschberg <ckirschberg@gmail.com>
2024-02-05 14:30:16 +0100

    entry

--- src/entry/entities/entry.entity.ts
+++ src/entry/entities/entry.entity.ts
@@ -1,1 +1,22 @@
-export class Entry {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Entry {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    amount: number
+
+    @Column()
+    date: Date
+
+    @Column()
+    currency: string
+
+    @Column()
+    name: string
+
+    @Column()
+    comment: string
+}
--- src/entry/entry.service.ts
+++ src/entry/entry.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEntryDto } from './dto/create-entry.dto';
 import { UpdateEntryDto } from './dto/update-entry.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Entry } from './entities/entry.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EntryService {
+
+  constructor(@InjectRepository(Entry) 
+    private entryRepository: Repository<Entry>) {}
+
+
   create(createEntryDto: CreateEntryDto) {
-    return 'This action adds a new entry';
+    return this.entryRepository.save(createEntryDto)
   }
 
   findAll() {
